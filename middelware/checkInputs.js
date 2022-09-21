f943e3612aaae87179db25edb83e06bf1446f39a
farouky <farouky@gmail.com>
2022-09-21 16:37:04 +0100

    Update checkInputs With REGEX

--- middelware/checkInputs.js
+++ middelware/checkInputs.js
@@ -1,9 +1,12 @@
 module.exports = (req, res, next) => {
-  if (req.method === "PUT" && req.file === undefined) { // si la requête est une requête PUT et qu'il n'y a pas de fichier
+  const regex = /^[a-zA-Z, àâäéèêëîïôöùûüÿçÂÊÎÔÛÄËÏÖÜÀÆÇÉÈŒÙ]+$/;
+  if (req.method === "PUT" && req.file === undefined) {
+    // si la requête est une requête PUT et qu'il n'y a pas de fichier
     const sauce = req.body;
     const { name, manufacturer, description, mainPepper } = sauce; // ES6 destructuring
     checkInput(req, res, next, name, manufacturer, description, mainPepper);
-  } else { // si la requête est une requête POST ou PUT et qu'il y a un fichier
+  } else {
+    // si la requête est une requête POST ou PUT et qu'il y a un fichier
     const sauce = JSON.parse(req.body.sauce);
     const { name, manufacturer, description, mainPepper } = sauce; // ES6 destructuration
     checkInput(req, res, next, name, manufacturer, description, mainPepper);
@@ -21,13 +24,23 @@ module.exports = (req, res, next) => {
       name.length >= 3 &&
       manufacturer.length >= 3 &&
       description.length >= 3 &&
-      mainPepper.length >= 3
+      mainPepper.length >= 3 &&
+      regex.test(name) &&
+      regex.test(manufacturer) &&
+      regex.test(description) &&
+      regex.test(mainPepper)
     ) {
       next();
     } else {
+      console.log(
+        regex.test(name),
+        regex.test(manufacturer),
+        regex.test(description),
+        regex.test(mainPepper)
+      );
       res.status(400).json({
         message:
-          "vous devez remplir tous les champs avec au moins 3caractéres !!!",
+          "vous devez remplir tous les champs avec au moins 3 caractéres sans utiliser de chiffres ni de symbole spéciale !!!",
       });
     }
   }
